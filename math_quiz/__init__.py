071469612123d407d5121bd368a1bca1b18e2849
Tim <tim.baum@fau.de>
2023-11-13 23:53:32 +0100

    Added the missing files to the repository to make it pip installable

---
+++ math_quiz/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 23:51:58 2023
+
+@author: timba
+"""
+
---
+++ setup.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 13 23:51:05 2023
+
+@author: timba
+"""
+
